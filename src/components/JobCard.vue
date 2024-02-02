5bf9aace5f714b2d54d9008b0ecff5083463c331
Sandra Almanzar <ingsandraalmanzar@gmail.com>
2024-02-02 17:54:40 -0500

    Agrego estilos al componente Card

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -1,48 +1,152 @@
 <template>
-    <div>
-        <div class="card-body p-2 p-md-3">
-            <div class="row align-items-center">
-                <div class="justify-content-center align-items-center col-md-1 col-3">
-                    <img v-bind:src="img" >
-                </div>
-                <div class="col-md-11 col-9">
-                    <div class="row align-items-center">
-                        <div class="mb-2 col">
-                            <h2>{{ job.name }}</h2>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <div class="row flex-colum">
-                <div>
-                    <span>{{ job.company }}</span>
-                </div>
-                <div>
-                    <span>{{ job.city }}</span>
-                </div>
-                <div>
-                    <span>{{ job.salary }}</span>
-                </div>
-                <div>
-                    <span>{{ job.vacancies }}</span>
-                </div>
-                <div>
-                    <span>{{ job.calendar }}</span>
-                </div>
-
-            </div>
-            
+  <div class="my-container mb-3 border">
+    <div class="card-body p-2 p-md-3">
+      <div class="row align-items-center">
+        <div class="justify-content-center align-items-center col-md-1 col-3">
+          <img
+            :src="job.img"
+            alt="Company logo"
+            width="70"
+            height="70"
+            class="rounded"
+          />
         </div>
+        <div class="col-md-11 col-9">
+          <div class="row align-items-center">
+            <div class="mb-2 col">
+              <a
+                href="#"
+                class="container-fluid text-dark d-inline d-md-inline-flex text-decoration-none"
+              >
+                <h1 class="font-weight-bold pl-md-2 pr-md-2 mb-0 pb-0">
+                  {{ job.name }}
+                </h1>
+                <section
+                  class="pl-md-2 d-inline d-md-inline-flex align-items-center text-truncate"
+                >
+                  <div
+                    class="text-truncate border border-dark text-dark text-capitalize m-1 p-1 rounded-2"
+                    style="font-size: 0.8em"
+                    v-for="(requirement, index) in job.requirements"
+                    :key="index"
+                  >
+                    <span>{{ requirement }}</span>
+                  </div>
+                </section>
+              </a>
+            </div>
+          </div>
+          <div
+            class="row pl-md-2 mt-1 flex-column flex-md-row"
+            style="font-size: 0.9em"
+          >
+            <div class="text-truncate text-capitalize col">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-buildings"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022M6 8.694 1 10.36V15h5zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5z"
+                />
+                <path
+                  d="M2 11h1v1H2zm2 0h1v1H4zm-2 2h1v1H2zm2 0h1v1H4zm4-4h1v1H8zm2 0h1v1h-1zm-2 2h1v1H8zm2 0h1v1h-1zm2-2h1v1h-1zm0 2h1v1h-1zM8 7h1v1H8zm2 0h1v1h-1zm2 0h1v1h-1zM8 5h1v1H8zm2 0h1v1h-1zm2 0h1v1h-1zm0-2h1v1h-1z"
+                />
+              </svg>
+              <span class="pe-2">{{ job.company }}</span>
+            </div>
+            <div class="text-truncate col">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-geo-alt"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"
+                />
+                <path
+                  d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"
+                />
+              </svg>
+              <span>{{ job.city }}</span>
+            </div>
+            <div class="text-truncate col">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-cash-stack"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4"
+                />
+                <path
+                  d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2z"
+                />
+              </svg>
+              <span>{{ job.salary }}</span>
+            </div>
+            <div class="text-truncate col">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-people"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"
+                />
+              </svg>
+              <span>{{ job.vacancies }}</span>
+            </div>
+            <div class="text-truncate col">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-calendar3"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2M1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857z"
+                />
+                <path
+                  d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2m3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2"
+                />
+              </svg>
+              <span>{{ job.calendar }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    
-  </template>
-  
-  <script>
-  export default {
-    name: 'job-card',
-    props:{
-        job: String
-    },
+  </div>
+</template>
+
+<script>
+export default {
+  name: "job-card",
+  props: {
+    job: String,
+  },
 };
-  </script>
+</script>
+<style>
+h2 {
+  font-family: sans-serif;
+}
+.my-container:hover {
+  box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.3);
+}
+</style>
