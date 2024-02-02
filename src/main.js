0c9fb8f6c8838cd0f085aafea6a33834f762e1be
Sandra Almanzar <ingsandraalmanzar@gmail.com>
2024-02-02 18:49:57 -0500

    Importo íconos Bootstrap

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
